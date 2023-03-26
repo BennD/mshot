bc375143fd83dc14e02a96eb6c5e9db9c5e86d39
Benno Doerr <bencayd@gmail.com>
2023-03-26 01:41:53 +0100

    added CLI

--- client/src/main.rs
+++ client/src/main.rs
@@ -1,14 +1,89 @@
-use std::fs;
+use anyhow::{anyhow, Result};
+use clap::Parser;
 use mshot_proto::screenshot::screenshot_handler_client::ScreenshotHandlerClient;
+use mshot_proto::screenshot::Screenshot;
+use std::fs;
+use std::path::PathBuf;
+use tokio::task::JoinSet;
+use tonic::transport::{Endpoint, Error};
 
+/// Command line interface
+#[derive(Parser)]
+#[command(author, version, about, long_about = None)]
+struct Cli {
+    /// Sets a custom output path (default: cwd)
+    #[arg(short, long, value_name = "DIR")]
+    out: Option<PathBuf>,
+
+    /// Targets to take screenshots of
+    #[arg(value_name = "NAME:IP[:PORT]", value_parser = parse_target)]
+    targets: Vec<Target>,
+}
+
+/// A target to take a screenshot of
+#[derive(Clone, Debug)]
+struct Target {
+    name: String,
+    endpoint: Endpoint,
+}
+
+/// Parse a target
+fn parse_target(input: &str) -> Result<Target, String> {
+    let (name, ip) = input
+        .split_once(|c| c == ':')
+        .ok_or("Missing delimiter ':' between name and ip")?;
+    let endpoint: Endpoint = ip.parse().map_err(|err: Error| err.to_string())?;
+    Ok(Target {
+        name: name.to_string(),
+        endpoint,
+    })
+}
+
+/// Main function
 #[tokio::main]
-async fn main() -> Result<(), Box<dyn std::error::Error>> {
-    let mut client = ScreenshotHandlerClient::connect("http://[::1]:50051").await?;
+async fn main() -> Result<()> {
+    let cli = Cli::parse();
 
-    let response = client.primary(()).await?;
+    // Check if we have targets
+    if cli.targets.is_empty() {
+        return Err(anyhow!("No targets specified"));
+    }
 
-    println!("RESPONSE={:?}", response);
-    fs::write("got_it.png", response.into_inner().image).unwrap();
+    // Spawn screenshot tasks
+    let mut set = JoinSet::new();
+    for target in cli.targets {
+        set.spawn(take_screenshot(target));
+    }
+
+    // Collect screenshots
+    let mut screenshots = Vec::new();
+    while let Some(result) = set.join_next().await {
+        screenshots.push(result?);
+    }
+
+    // Save screenshots
+    let output = cli.out.unwrap_or_else(|| ".".into());
+    for screenshot in screenshots {
+        let (
+            name,
+            Screenshot {
+                width: _width,
+                height: _height,
+                image,
+            },
+        ) = screenshot?;
+        let path = output.join(format!("{}.png", name));
+        fs::write(&path, image)?;
+        println!("Saved screenshot of {} to {}", name, path.display());
+    }
 
     Ok(())
+}
+
+/// Take a screenshot of a target
+async fn take_screenshot(target: Target) -> Result<(String, Screenshot)> {
+    let Target { name, endpoint } = target;
+    let mut client = ScreenshotHandlerClient::connect(endpoint).await?;
+    let response = client.primary(()).await?;
+    Ok((name, response.into_inner()))
 }
--- server/src/main.rs
+++ server/src/main.rs
@@ -1,20 +1,45 @@
-use anyhow::Result;
-use screenshots::{DisplayInfo, Screen};
-use tonic::{Request, Response, Status};
-use tonic::transport::Server;
+use anyhow::{anyhow, Result};
+use clap::Parser;
+use mshot_proto::screenshot::screenshot_handler_server::{
+    ScreenshotHandler, ScreenshotHandlerServer,
+};
 use mshot_proto::screenshot::Screenshot;
-use mshot_proto::screenshot::screenshot_handler_server::{ScreenshotHandler, ScreenshotHandlerServer};
+use screenshots::{DisplayInfo, Screen};
+use std::net::{AddrParseError, SocketAddr};
+use tonic::transport::Server;
+use tonic::{Request, Response, Status};
 
+/// The command line interface
+#[derive(Parser)]
+#[command(author, version, about, long_about = None)]
+struct Cli {
+    #[arg(value_parser = parse_addr)]
+    address: SocketAddr,
+}
+
+/// Parse a socket address
+fn parse_addr(input: &str) -> Result<SocketAddr, String> {
+    input.parse().map_err(|err: AddrParseError| err.to_string())
+}
+
+/// The screenshot provider
 #[derive(Debug, Default)]
 struct ScreenshotProvider;
 
+/// Implement the screenshot handler
 #[tonic::async_trait]
 impl ScreenshotHandler for ScreenshotProvider {
-    async fn primary(&self, request: Request<()>) -> std::result::Result<Response<Screenshot>, Status> {
+    async fn primary(
+        &self,
+        request: Request<()>,
+    ) -> std::result::Result<Response<Screenshot>, Status> {
         println!("Got a request: {:?}", request);
 
-        let screen = get_primary_screen().unwrap();
-        let image = screen.capture().unwrap();
+        let screen = get_primary_screen()
+            .map_err(|err| Status::internal(format!("Failed to get primary screen: {}", err)))?;
+        let image = screen.capture().map_err(|err| {
+            Status::internal(format!("Failed to capture primary screen: {}", err))
+        })?;
         let screenshot = Screenshot {
             width: image.width(),
             height: image.height(),
@@ -25,20 +50,30 @@ impl ScreenshotHandler for ScreenshotProvider {
     }
 }
 
+/// Main function
 #[tokio::main]
-async fn main() -> Result<(), Box<dyn std::error::Error>> {
-    let addr = "[::1]:50051".parse()?;
-    let service = ScreenshotProvider::default();
+async fn main() -> Result<()> {
+    let cli = Cli::parse();
+
+    let addr = cli.address;
+    let screenshot_provider = ScreenshotProvider::default();
+
+    println!("Listening on {}", addr);
 
     Server::builder()
-        .add_service(ScreenshotHandlerServer::new(service))
+        .add_service(ScreenshotHandlerServer::new(screenshot_provider))
         .serve(addr)
-        .await?;
+        .await
+        .map_err(|err| anyhow!("Failed to start server: {}", err))?;
 
     Ok(())
 }
 
+/// Get the primary screen
 fn get_primary_screen() -> Result<Screen> {
-    let primary = DisplayInfo::all()?.into_iter().find(|info| info.is_primary).unwrap();
+    let primary = DisplayInfo::all()?
+        .into_iter()
+        .find(|info| info.is_primary)
+        .ok_or_else(|| anyhow!("No primary display found"))?;
     Ok(Screen::new(&primary))
 }
