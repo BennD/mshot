42b6f831bd52a218118a2f5f2813f71bec342a48
BennD <bencayd@gmail.com>
2023-03-27 18:00:57 +0200

    rewritten server code (#6)

--- client/src/main.rs
+++ client/src/main.rs
@@ -16,7 +16,7 @@ struct Cli {
     out: Option<PathBuf>,
 
     /// Targets to take screenshots of
-    #[arg(value_name = "NAME:IP[:PORT]", value_parser = parse_target)]
+    #[arg(value_name = "NAME:ENDPOINT", value_parser = parse_target)]
     targets: Vec<Target>,
 }
 
@@ -29,13 +29,12 @@ struct Target {
 
 /// Parse a target
 fn parse_target(input: &str) -> Result<Target, String> {
-    let (name, ip) = input
+    let (name, endpoint) = input
         .split_once(|c| c == ':')
-        .ok_or("Missing delimiter ':' between name and ip")?;
-    let endpoint: Endpoint = ip.parse().map_err(|err: Error| err.to_string())?;
+        .ok_or("Missing delimiter ':' between name and endpoint")?;
     Ok(Target {
         name: name.to_string(),
-        endpoint,
+        endpoint: endpoint.parse().map_err(|err: Error| err.to_string())?,
     })
 }
 
@@ -49,41 +48,54 @@ async fn main() -> Result<()> {
         return Err(anyhow!("No targets specified"));
     }
 
-    // Spawn screenshot tasks
-    let mut set = JoinSet::new();
+    // Ensure output folder exists
+    if let Some(out) = &cli.out {
+        fs::create_dir_all(out)?;
+    }
+
+    // Spawn screenshot tasks into a JoinSet which can later be collected into a Vec
+    let mut join_set = JoinSet::new();
     for target in cli.targets {
-        set.spawn(take_screenshot(target));
+        let Target { name, endpoint } = target;
+
+        // Spawn screenshot task
+        join_set.spawn(async move {
+            // Create client
+            let mut client = ScreenshotHandlerClient::connect(endpoint)
+                .await
+                .map_err(|err| anyhow!("Failed to connect to {}: {}", name, err))?;
+
+            // Take screenshot
+            let screenshot = client
+                .primary(())
+                .await
+                .map_err(|err| anyhow!("Failed to take screenshot of {}: {}", name, err))?
+                .into_inner();
+
+            // Return name / screenshot
+            Ok::<(String, Screenshot), anyhow::Error>((name, screenshot))
+        });
     }
 
     // Collect screenshots
     let mut screenshots = Vec::new();
-    while let Some(result) = set.join_next().await {
-        screenshots.push(result?);
+    while let Some(screenshot) = join_set.join_next().await {
+        screenshots.push(screenshot);
     }
 
-    // Save screenshots
-    let output = cli.out.unwrap_or_else(|| ".".into());
+    // Save screenshots to disk
     for screenshot in screenshots {
-        let (
-            name,
-            Screenshot {
-                width: _width,
-                height: _height,
-                image,
-            },
-        ) = screenshot?;
-        let path = output.join(format!("{}.png", name));
-        fs::write(&path, image)?;
-        println!("Saved screenshot of {} to {}", name, path.display());
+        // Unwrap screenshot
+        let (name, screenshot) =
+            screenshot.map_err(|err| anyhow!("Screenshot task failed: {}", err))??;
+
+        // Create output path
+        let mut out = cli.out.clone().unwrap_or_else(|| PathBuf::from("."));
+        out.push(format!("{}.png", name));
+
+        // Write screenshot to disk
+        fs::write(out, screenshot.image)?;
     }
 
     Ok(())
-}
-
-/// Take a screenshot of a target
-async fn take_screenshot(target: Target) -> Result<(String, Screenshot)> {
-    let Target { name, endpoint } = target;
-    let mut client = ScreenshotHandlerClient::connect(endpoint).await?;
-    let response = client.primary(()).await?;
-    Ok((name, response.into_inner()))
 }
